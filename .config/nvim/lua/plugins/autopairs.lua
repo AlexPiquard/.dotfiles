eb60243a6a01103d391016f043bc1e48d93218f9
Alex Piquard <alex.piquard@hotmail.com>
2025-11-12 18:26:27 +0100

    use mini.pairs instead of nvim-autopairs

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,10 +1,21 @@
 return {
-	-- autopairing of (){}[] etc
+	-- Auto pairs
+	-- Automatically inserts a matching closing character
+	-- when you type an opening character like `"`, `[`, or `(`.
 	{
-		"windwp/nvim-autopairs",
+		"nvim-mini/mini.pairs",
+		event = "VeryLazy",
 		opts = {
-			fast_wrap = {},
-			disable_filetype = { "TelescopePrompt", "snacks_picker_list", "vim" },
+			modes = { insert = true, command = true, terminal = false },
+			-- skip autopair when next character is one of these
+			skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+			-- skip autopair when the cursor is inside these treesitter nodes
+			skip_ts = { "string" },
+			-- skip autopair when next character is closing pair
+			-- and there are more closing pairs than opening pairs
+			skip_unbalanced = true,
+			-- better deal with markdown code blocks
+			markdown = true,
 		},
 	},
 }
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -43,13 +43,6 @@ return {
 				end,
 			},
 
-			{
-				"windwp/nvim-autopairs",
-				opts = {
-					fast_wrap = {},
-					disable_filetype = { "TelescopePrompt", "snacks_picker_input", "vim" },
-				},
-			},
 			"onsails/lspkind.nvim",
 		},
 
