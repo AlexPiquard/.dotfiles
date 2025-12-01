688e1a08b80922f9b085a1a7522e2d8fa13d2e4a
Alex Piquard <alex.piquard@hotmail.com>
2025-12-01 17:40:04 +0100

    fix todo-comments errors

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -1,19 +1,17 @@
 return {
-  {
-    "folke/todo-comments.nvim",
-    event = "VimEnter",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-			signs = true,
-      highlight = {
-        pattern = [[.*<(KEYWORDS)\s*]]
-      },
-      search = {
-        pattern = [[\b(KEYWORDS)\b]]
-      }
-    },
-    keys = {
-      { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" }
-    },
-  }
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+		keys = {
+			{
+				"<leader>st",
+				function()
+					Snacks.picker.todo_comments()
+				end,
+				desc = "Todo",
+			},
+		},
+	},
 }
