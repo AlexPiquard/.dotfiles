15c144e28fd63210bf80b7a9244fea31bfc802bd
Alex Piquard <alex.piquard@hotmail.com>
2026-01-02 14:21:56 +0100

    disable NOTE and INFO from todo-comments

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -2,12 +2,28 @@ return {
 	"folke/todo-comments.nvim",
 	event = "VimEnter",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	opts = {},
+	opts = {
+		merge_keywords = false,
+		keywords = {
+			FIX = {
+				icon = " ", -- icon used for the sign, and in search results
+				color = "error", -- can be a hex color, or a named color (see below)
+				alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+				-- signs = false, -- configure signs for some keywords individually
+			},
+			TODO = { icon = " ", color = "info" },
+			HACK = { icon = " ", color = "warning" },
+			WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+			PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+			-- NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+		},
+	},
 	keys = {
 		{
 			"<leader>st",
 			function()
-				Snacks.picker.todo_comments() -- TODO: not working
+				Snacks.picker.todo_comments()
 			end,
 			desc = "List Todos",
 		},
