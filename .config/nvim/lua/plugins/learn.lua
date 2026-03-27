3bb3bda0f14ae61db24dc4f0dfe101520a7cfb54
Alex Piquard <alex.piquard@hotmail.com>
2026-03-27 17:56:12 +0100

    disable hardtime in fyler

--- .config/nvim/lua/plugins/learn.lua
+++ .config/nvim/lua/plugins/learn.lua
@@ -3,5 +3,11 @@ return {
 	"m4xshen/hardtime.nvim",
 	event = "BufReadPost",
 	dependencies = { "MunifTanjim/nui.nvim" },
-	opts = {},
+	opts = {
+		disabled_filetypes = {
+			lazy = true,
+			["dapui*"] = true,
+			fyler = true,
+		},
+	},
 }
