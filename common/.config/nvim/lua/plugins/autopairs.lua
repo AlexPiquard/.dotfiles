bf452899f0a1e29fe974fe322f105e8983998f84
Alex Piquard <alex.piquard@hotmail.com>
2026-04-30 17:11:50 +0200

    fix autopairs in rust

--- common/.config/nvim/lua/plugins/autopairs.lua
+++ common/.config/nvim/lua/plugins/autopairs.lua
@@ -7,21 +7,11 @@ return {
 	opts = {
 		-- alt-e to add the closing thing after word
 		fast_wrap = {},
-		disable_filetype = { "markdown", "snacks_picker_input" },
+		disable_filetype = { "markdown", "snacks_picker_input", "opencode" },
 		ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=],
 		-- disable in string
 		enable_bracket_in_quote = false,
 		-- (|)) -> ((|))
 		enable_check_bracket_line = true,
 	},
-	config = function(_, opts)
-		local autopairs = require("nvim-autopairs")
-		local cond = require("nvim-autopairs.conds")
-		local Rule = require("nvim-autopairs.rule")
-
-		autopairs.setup(opts)
-
-		-- disable ' for rust
-		autopairs.add_rule(Rule("'", "'"):with_pair(cond.not_filetypes({ "rust" })))
-	end,
 }
